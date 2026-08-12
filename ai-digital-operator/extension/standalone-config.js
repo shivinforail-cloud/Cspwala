2e84d95f8ae0e2862386b3d181878e17aa2365d4
shivinforail-cloud <shivinforail@gmail.com>
2026-08-13 04:00:20 +0530

    Add standalone Shadow Agent Firebase project config

---
+++ ai-digital-operator/extension/standalone-config.js
@@ -0,0 +1,15 @@
+const SHADOW_AGENT_FIREBASE = Object.freeze({
+  apiKey: "AIzaSyBFk9ZdZ5oPCbx6X1yzXeFC6hB6wSAtyqQ",
+  authDomain: "quick-response-h7ttf.firebaseapp.com",
+  databaseURL: "https://quick-response-h7ttf-default-rtdb.firebaseio.com",
+  projectId: "quick-response-h7ttf",
+  storageBucket: "quick-response-h7ttf.firebasestorage.app",
+  messagingSenderId: "854494741612",
+  appId: "1:854494741612:web:bed658290d89ad4aef4449",
+  measurementId: "G-6FLE5T79G6"
+});
+
+const SHADOW_AGENT_BLOCKED_PROJECTS = Object.freeze([
+  "studio-9554958219-678f5",
+  "communityapp-dfaff"
+]);
